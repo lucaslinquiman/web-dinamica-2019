d0950c2d8198132570d54fadcfb0a79863b05dc5
Lucas Linquiman <lucaslinquiman@yahoo.com.ar>
2019-04-09 00:12:33 +0000

    Actualizando index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
 class Alumno{
+    protected $nombre;
+    protected $apellido;
     
+    public function __construct(string $nombre, string $apellido){
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+    }
+    
+    public function getNombre(): string{
+        return $this->nombre;
+    }
 }
 
 echo "Hola Mundo"; // prueba
